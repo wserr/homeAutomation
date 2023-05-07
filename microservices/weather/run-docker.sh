76a242f58b68c03180a9b3a181a72f15b956d067
wserr <serruys.willem@hotmail.com>
2023-05-07 13:43:37 +0200

    Modified docker run script

--- microservices/weather/run-docker.sh
+++ microservices/weather/run-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+file=logs.$(date "+%F").txt
 
-docker run --env RUST_LOG=info --env-file ./.env registry.willemserruys.com/weather-microservice:latest &> logs.txt 
+docker run --env RUST_LOG=info --env-file ./.env registry.willemserruys.com/weather-microservice:latest &> $file
