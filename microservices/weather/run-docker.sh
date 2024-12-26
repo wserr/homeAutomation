2ca39264d12edd88adec00b1ac1929020a24b99e
wserr <serruys.willem@hotmail.com>
2024-12-26 09:36:09 +0100

    Weather service installation

--- microservices/weather/run-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-file=/home/willem/repos/homeAutomation/microservices/weather/logs.$(date "+%F").txt
-
-docker run --env RUST_LOG=info --env-file /home/willem/repos/homeAutomation/microservices/weather/.env registry.willemserruys.com/weather-microservice:latest &>> $file
