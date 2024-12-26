d33fbf8dd0ced0c0027b774c7d617ef18666cca2
wserr <serruys.willem@hotmail.com>
2024-12-26 10:54:49 +0100

    Cleanup

--- microservices/weather/scripts/run-docker.sh
+++ microservices/weather/scripts/run-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Make sure the weather-service-image exists locally (e.g. by running the build-docker script)
-docker run --env RUST_LOG=info --env-file /home/pi/repos/homeAutomation/microservices/weather/.env.production weather-service:latest
+docker run --env RUST_LOG=info --env-file /home/pi/repos/homeAutomation/microservices/weather/environment-variables/.env.production weather-service:latest
