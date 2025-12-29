de6558ec7ee4926837f245a1e19c577ed6b4186c
willem.serruys <willemserruys@proton.me>
2025-12-29 06:06:46 +0100

    chore: updated docker

--- microservices/power_control/Program.cs
+++ microservices/power_control/Program.cs
@@ -6,10 +6,13 @@ var app = builder.Build();
 
 app.MapPost(
         "/high_power_alert",
-        (object body) =>
+        async (AlertBody body) =>
         {
             Console.WriteLine("high power alert was received.");
             Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(body));
+
+	    using var client = new HttpClient();
+	    await client.GetAsync("http://192.168.1.121/control?cmd=heatpump&set_power_mode=off");
             return new OkResult();
         }
     )
@@ -26,3 +29,8 @@ app.MapPost(
     .WithName("Low Power Alert");
 
 app.Run();
+
+record AlertBody
+{
+	string status;
+}
